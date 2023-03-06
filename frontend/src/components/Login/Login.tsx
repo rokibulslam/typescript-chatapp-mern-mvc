a1ad228b06956dbef234ffac5c48f8b86f83e91b
rokibulslam <rokibulslam6@gmail.com>
2023-03-06 08:24:42 +0600

    login ad register ui, integreted with backend

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,21 +1,55 @@
+import axios from 'axios';
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from "react-toastify";
+
 
 const Login = () => {
+  const navigate = useNavigate();
     const [email, setEmail] = useState<string>("");
-    const [Password, setPassword] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [loading, setLoading] = useState(false);
     const [show, setShow] = useState<boolean>(false);
-    const handleShow = () => setShow(!show);
-    const handleForm = (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-    };
+    
+  const handleShow = () => setShow(!show);
+  
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    if ( !email || !password) {
+      toast("Please fill all the filleds");
+      setLoading(false);
+      return;
+    }
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        "/api/login",
+        {  email, password},
+        config
+      );
+      toast("Login Successfull");
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      navigate("/chats");
+    } catch (error) {
+      toast("Error");
+      setLoading(false);
+    }
+  };
   return (
     <div className="">
-      <form action="" onSubmit={(e) => handleForm(e)} className="space-y-3">
+      <form action="" onSubmit={(e) => handleSubmit(e)} className="space-y-3">
         <div className="flex flex-col">
           <label htmlFor="">Enter Your Email</label>
           <input
             onChange={(e) => setEmail(e.target.value)}
             className="rounded-lg h-10 bg-teal-100"
+            value={email}
             type="text"
             required
           />
@@ -26,6 +60,7 @@ const Login = () => {
             <input
               onChange={(e) => setPassword(e.target.value)}
               className="rounded-lg h-10 bg-teal-100 "
+              value={password}
               type={show ? "text" : "password"}
               required
             />
@@ -44,9 +79,11 @@ const Login = () => {
           Login
         </button>
         <button
-                  className="w-full bg-green-600 text-yellow-50 py-1 rounded-2xl"
-                  onClick={() => { setEmail("guest@email.com"); setPassword("123456")}}
-                
+          className="w-full bg-green-600 text-yellow-50 py-1 rounded-2xl"
+          onClick={() => {
+            setEmail("guest@email.com");
+            setPassword("123456");
+          }}
           type="submit"
         >
           Guest
--- frontend/src/components/Register/Register.tsx
+++ frontend/src/components/Register/Register.tsx
@@ -1,21 +1,77 @@
+import axios from "axios";
 import React, { useState } from "react";
-
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 const Register = () => {
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [confirmPass, setConfirmPass] = useState<string>("");
-  const [Password, setPassword] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   const [picture, setPicture] = useState(null);
   const [show, setShow] = useState<boolean>(false);
-
+  const [loading, setLoading]=useState(false)
   const handleShow = () => setShow(!show);
-  const handlePicture = (e:File|undefined) => {console.log(e);};
-  const handleForm = (e: React.FormEvent<HTMLFormElement>) => {
+  const navigate = useNavigate()
+  const handlePicture = (picture: File | undefined) => {
+    setLoading(true)
+    if (picture === undefined) {
+      toast("Insert Image");
+    }
+    if (picture?.type === 'image/jpeg' || picture?.type === 'image/png') {
+      const data = new FormData();
+      data.append("file", picture);
+      data.append("upload_preset", "chat_app");
+      data.append("cloud_name", "di2tro0se");
+      fetch("https://api.cloudinary.com/v1_1/di2tro0se/image/upload", {
+        method: "post",
+        body:data
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPicture(data.url.toString());
+        
+          toast("Success");
+          setLoading(false);
+        }).catch((err) => {
+          console.log(err);
+          setLoading(false);
+          toast("Image upload failed!");
+        }) 
+      
+    } 
+  };
+  const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setLoading(true)
+    if (!name || !email || !confirmPass) {
+      toast("Please fill all the filleds")
+      setLoading(false);
+      return
+    } if (password !== confirmPass) {
+      toast("Password does not match")
+       setLoading(false);
+      return
+    }
+    try {
+      const config = {
+        headers: {
+          "Content-type":"application/json"
+        }
+      }
+      const { data } = await axios.post("/api/register", { name, email, password, picture }, config)
+      toast("Registration Successfull");
+      localStorage.setItem("userInfo", JSON.stringify(data))
+      setLoading(false)
+      navigate("/chats") 
+    } catch (error) {
+      toast("Error")
+      setLoading(false)
+    }
   };
   return (
     <div className="">
-      <form action="" onSubmit={(e) => handleForm(e)} className="space-y-3">
+      <ToastContainer />
+      <form action="" onSubmit={(e) => handleSubmit(e)} className="space-y-3">
         <div className="flex flex-col">
           <label htmlFor="">Enter Your Name</label>
           <input
@@ -71,12 +127,22 @@ const Register = () => {
             required
           />
         </div>
-        <button
-          className="w-full bg-red-500 text-yellow-50 py-1 rounded-2xl"
-          type="submit"
-        >
-          Register
-        </button>
+        {loading ? (
+          <button
+            className="w-full bg-black-500 text-yellow-50 py-1 rounded-2xl"
+            type="submit"
+            disabled
+          >
+            Register
+          </button>
+        ) : (
+          <button
+            className="w-full bg-red-500 text-yellow-50 py-1 rounded-2xl"
+            type="submit"
+          >
+            Register
+          </button>
+        )}
       </form>
     </div>
   );
